afb463a6b935670369415b99e83e0b1d042cb72d
Irvins <irvins.cassull@gmail.com>
2019-02-11 19:02:07 +0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist'
+  },
+  module: {
+    rules: [{
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        miniCssExtractPlugin.loader,
+        {
+          loader: "css-loader",
+          options: { sourceMap: true }
+        }, {
+          loader: "postcss-loader",
+          options: { sourceMap: true, config: {path: 'src/js/postcss.config.js'} }
+        }, 
+      ]
+    }, {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        miniCssExtractPlugin.loader,
+        {
+          loader: "css-loader",
+          options: { sourceMap: true }
+        }, {
+          loader: "postcss-loader",
+          options: { sourceMap: true, config: {path: 'src/js/postcss.config.js'} }
+        }, {
+          loader: "sass-loader",
+          options: { sourceMap: true }
+        }
+      ]
+    }, {
+      test: /\.pug$/,
+      use: [{
+        loader: 'pug-loader',
+        options: { pretty: true }
+      }]
+    }, {
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [{
+          loader: 'file-loader',
+          options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+          }
+      }]
+    }]
+  },
+  devServer: {
+    overlay: true
+  },
+  plugins: [
+    new miniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.pug'
+    })
+  ]
+}
